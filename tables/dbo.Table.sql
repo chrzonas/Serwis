8663ca816f75a6dcc4554580571648104e67a371
chrzonas <chrzonas@o2.pl>
2020-05-16 22:09:35 +0200

    add clients table

---
+++ tables/dbo.Table.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[Clients]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(20) NULL, 
+    [Surname] NCHAR(20) NULL, 
+    [Tel] NCHAR(15) NULL, 
+    [Street] NCHAR(30) NULL, 
+    [House number] NCHAR(10) NULL, 
+    [Apartment number] NCHAR(10) NULL, 
+    [City] NCHAR(30) NULL, 
+    [Zip code] NCHAR(10) NULL, 
+    [Post office] NCHAR(30) NULL
+)
